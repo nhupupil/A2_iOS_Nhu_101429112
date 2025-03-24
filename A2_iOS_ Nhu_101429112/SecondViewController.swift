894cf4976f6a851f6d0cd30bb92bef287f73b1f9
nhunique <lyhuynhyennhu01@gmail.com>
2025-03-24 15:58:03 -0400

    setup first and second screen UI

---
+++ A2_iOS_ Nhu_101429112/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  A2_iOS_ Nhu_101429112
+//
+//  Created by Huynh Yen Nhu Ly on 2025-03-24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
